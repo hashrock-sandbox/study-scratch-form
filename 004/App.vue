2e1d0f51170b2e7f3529f7e19cc141943ade2cac
hashrock <hashrock@users.noreply.github.com>
2020-02-01 13:42:52 +0900

    add a11y

---
+++ 004/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="app">
+    <select>
+      <option>Option 01</option>
+      <option>Option 02</option>
+      <option>Option 03</option>
+      <option>Option 04</option>
+      <option>Option 05</option>
+    </select>
+
+    <my-select :options="options" v-model="value"></my-select>
+  </div>
+</template>
+<script>
+import MySelect from "./MySelect.vue";
+
+export default {
+  components: {
+    MySelect
+  },
+  data() {
+    return {
+      value: "Option 01",
+      options: [
+        { value: "Option 01", id: "Option01" },
+        { value: "Option 02", id: "Option02" }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+body,
+html {
+  height: 100%;
+}
+body {
+  font-size: 16px;
+  margin: 0;
+}
+
+.app {
+  height: 100%;
+  display: flex;
+  width: 300px;
+  margin: 0 auto;
+  justify-content: space-around;
+  align-items: center;
+}
+
+select {
+  font-size: inherit;
+}
+</style>
---
+++ 004/MyOption.vue
@@ -0,0 +1,35 @@
+<template>
+  <li
+    @mousedown="select($event)"
+    :class="{selected: selected}"
+    :aria-selected="selected"
+    class="option"
+    role="option"
+    :id="option.id"
+  >{{option.value}}</li>
+</template>
+
+<script>
+export default {
+  props: { selected: Boolean, option: Object },
+  methods: {
+    select() {
+      this.$emit("select", this.option);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.option {
+  line-height: 1.5em;
+  padding: 0 0.5em;
+  cursor: default;
+}
+.option:hover {
+  background: #ddd;
+}
+.selected {
+  background: #aaa;
+}
+</style>
