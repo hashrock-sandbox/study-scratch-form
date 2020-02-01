796c5c7950cfb15707a472748d02b9c9fced89b6
hashrock <hashrock@users.noreply.github.com>
2020-02-01 14:06:16 +0900

    000

---
+++ 000/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="app">
+    <select>
+      <option>Option 01</option>
+      <option>Option 02</option>
+      <option>Option 03</option>
+      <option>Option 04</option>
+      <option>Option 05</option>
+    </select>
+
+    <my-select></my-select>
+  </div>
+</template>
+<script>
+import MySelect from "./MySelect.vue";
+
+export default {
+  components: {
+    MySelect
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+body,
+html {
+  height: 100%;
+}
+body {
+  font-size: 16px;
+  margin: 0;
+}
+
+.app {
+  height: 100%;
+  display: flex;
+  width: 300px;
+  margin: 0 auto;
+  justify-content: space-around;
+  align-items: center;
+}
+
+select {
+  font-size: inherit;
+}
+</style>
---
+++ 000/MySelect.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="select" @mousedown="open = true" @blur="open = false" tabindex="0">
+    <div class="select__label">
+      Option 01
+      ▼
+    </div>
+    <ul class="options" v-if="open">
+      <li class="option selected">Option 01</li>
+      <li class="option">Option 02</li>
+      <li class="option">Option 03</li>
+      <li class="option">Option 04</li>
+      <li class="option">Option 05</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      open: false
+    };
+  }
+};
+</script>
+
+<style scoped>
+.select {
+  position: relative;
+  font-family: system-ui;
+  border: 1px solid #999;
+  padding: 0.25em 0.5em;
+  line-height: 0.9em;
+  border-radius: 4px;
+  cursor: default;
+  display: flex;
+  align-items: center;
+  box-sizing: border-box;
+}
+.options {
+  position: absolute;
+  left: 0;
+  background: white;
+  border: 1px solid #999;
+  white-space: nowrap;
+  border-radius: 4px;
+  top: 100%;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.option {
+  line-height: 1.5em;
+  padding: 0 0.5em;
+  cursor: default;
+}
+.option:hover {
+  background: #ddd;
+}
+.selected {
+  background: #aaa;
+}
+</style>
